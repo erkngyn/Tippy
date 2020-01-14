f5c76ed1c7ed9c83eb25fc8ef00821d3d29f7260
Erik Nguyen <nguyenec@uci.edu>
2020-01-14 13:22:45 -0800

    Initial Commit

---
+++ Tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tippy
+//
+//  Created by Erik Nguyen on 1/14/20.
+//  Copyright © 2020 Erik Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
